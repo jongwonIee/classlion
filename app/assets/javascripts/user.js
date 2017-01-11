fef376986f1a368d68082c2a35376e1fef6b86a4
missAHNG <awr0607@gmail.com>
2017-01-11 17:26:06 +0900

    comment refactoring

--- app/assets/javascripts/user.js
+++ app/assets/javascripts/user.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-
+//변수정의
     var email = $('#user_email'),
 
         password = $('#user_password'),
@@ -22,6 +22,7 @@ $(document).ready(function(){
 
         newUserForm = $('#join_btn');
 
+//전체적으로 적용될 코드
     $('.error').hide(); //에러메세지가 출력되는 span태그 숨기기
 
     //input에 focus가 왔을 때, css변경 (불투명 색상)
@@ -31,8 +32,12 @@ $(document).ready(function(){
         $(this).removeClass('focus');
     });
 
-//여기서부터 유효성 체크
-    //이메일 체크 - 이메일 포커스를 벗어났을 때 유효성 체크-----------------------------------------------
+
+
+//여기서부터 유효성 체크 >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+//이메일 체크-----------------------------------------------
+    //이메일 함수정의
     function validateEmail(){
         var emailVal = email.val();
         var e = /^[-a-z0-9~!$%^&*_=+}{\'?]+(\.[-a-z0-9~!$%^&*_=+}{\'?]+)*@([a-z0-9_][-a-z0-9_]*(\.[-a-z0-9_]+)*\.(aero|arpa|biz|com|coop|edu|gov|info|int|mil|museum|name|net|org|pro|travel|mobi|[a-z][a-z])|([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}))(:[0-9]{1,5})?$/;
@@ -66,13 +71,16 @@ $(document).ready(function(){
                 }
             });
         }
-    }
+    }//이메일 함수 끝
 
-    //이메일 관련 함수호출
+
+    //이메일 함수호출
     email.blur(function(){
         validateEmail(); //반환값이 true이면 이메일 형식에 맞는 것
     });
 
+
+    //이메일 예외사항 처리
     email.focus(function(){ //입력값이 들어오면 '필수정보'안내 없앰
         emailError.hide();
         email[0].parentNode.parentNode.parentNode.style.borderColor = "#fff";
@@ -81,29 +89,27 @@ $(document).ready(function(){
 
 
 
-    //비밀번호 체크 - 키보드 칠 때 동적으로 체크---------------------------------------------------------
-    function validatePassword(){
+//비밀번호 체크 ---------------------------------------------------------
+    //비밀번호 함수정의
+    function validatePassword(){ //키보드 칠 때 동적으로 체크
         var passwordVal = password.val();
         var passwordConfVal = passwordConfirmation.val();
 
-        //이미 '비밀번호 확인'이 작성되어 있는 경우
-        if(passwordConfVal){
+        if(passwordConfVal){ //이미 '비밀번호 확인'이 작성되어 있는 경우
             if(passwordVal.length >= 8) { //'비밀번호' 길이가 8이상일 때
                 if (passwordVal === passwordConfVal) { //'비밀번호 확인'과 '비밀번호'가 같은경우
                     passwordConfError.text("일치").addClass('green');
                     passwordConfirmation[0].parentNode.parentNode.style.borderColor = "#fff";
-                } else {//'비밀번호 확인'과 '비밀번호'가 같지 않은 경우
+                    return true;
+                } else { //'비밀번호 확인'과 '비밀번호'가 같지 않은 경우
                     passwordConfError.show().text("불일치").removeClass('green').addClass('red');
                     passwordConfirmation[0].parentNode.parentNode.style.borderColor = "#ff5a5f";
                 }
             }else{ //'비밀번호' 길이가 8이상이 아닐 때
-                passwordConfError.hide();//'일치', '불일치'에 대한 안내는 하지 않는다 (8이상일 때만 함!)
+                passwordConfError.hide(); //'일치', '불일치'에 대한 안내는 하지 않는다 (8이상일 때만 함!)
             }
-
         }
-
-        //길이가 1이상 8보다 작은경우
-        if(passwordVal.length >= 1 && passwordVal.length < 8){
+        if(passwordVal.length >= 1 && passwordVal.length < 8){ //길이가 1이상 8보다 작은경우
             passwordConfError.hide();
             passwordValidate.show().text("조금만 더! 비밀번호는 8자 이상이에요!").addClass('red');
             password[0].parentNode.parentNode.style.borderColor = "#ff5a5f";
@@ -111,14 +117,46 @@ $(document).ready(function(){
             passwordValidate.hide(); //"조금만 더! 비밀번호는 8자 이상이에요!" 메세지 사라짐
             password[0].parentNode.parentNode.style.borderColor = "#fff"; // 선색은 흰색
         }
+    } //비밀번호 함수 1 끝
 
-    }
+    function validatePasswordAndPasswordConf(){ //비밀번호와 비밀번호확인 포커스 벗어났을 때
+        passwordVal = password.val();
+        passwordConfVal = passwordConfirmation.val();
+        passwordConfError.show();
+
+        if(!passwordVal) {
+            passwordConfError.hide();
+            passwordValidate.show().text("필수정보 입니다.").addClass('red');
+            password[0].parentNode.parentNode.style.borderColor = "#ff5a5f";
+        }
+        if(passwordVal.length >= 8) { //비밀번호가 8이상일 때
+            if (passwordVal === '') { //'비밀번호'가 빈칸이면
+                passwordConfError.hide(); //'일치', '불일치'에 대한 안내 안함
+            }else if (passwordVal === passwordConfVal) {
+                passwordConfError.text("일치").addClass('green');
+                passwordConfirmation[0].parentNode.parentNode.style.borderColor = "#fff";
+                return true;
+            }else {
+                passwordConfError.show().text("불일치").removeClass('green').addClass('red');
+                passwordConfirmation[0].parentNode.parentNode.style.borderColor = "#ff5a5f";
+            }
+        }else{ //비밀번호가 8보다 작을 때
+            passwordConfError.hide(); //'일치', '불일치'에 대한 안내는 하지 않는다
+        }
+    }//비밀번호 함수 2 끝
+
 
     //비밀번호 관련 함수호출
     password.keyup(function(){ //키보드 칠 때 동적으로 작동(상단에 위치한 함수 호출)
         validatePassword();
     });
 
+    passwordInputs.blur(function(){ //'비밀번호'와 '비밀번호 확인' 동시에 포커스 벗어난 경우
+        validatePasswordAndPasswordConf();
+    });
+
+
+    //비밀번호 예외사항 처리
     password.focus(function(){ //비밀번호에 포커스가 잡히면 에러메세지 없앰, 흰색 (비밀번호에서 거꾸로 탭할때의 버그 없애기 위함
         passwordValidate.hide();
         password[0].parentNode.parentNode.style.borderColor = "#fff";
@@ -143,37 +181,12 @@ $(document).ready(function(){
     });
 
 
-    //'비밀번호'와 '비밀번호 확인' 동시에 포커스 벗어난 경우
-    passwordInputs.blur(function(){
-        passwordVal = password.val();
-        passwordConfVal = passwordConfirmation.val();
-        passwordConfError.show();
 
-        if(!passwordVal) {
-            passwordConfError.hide();
-            passwordValidate.show().text("필수정보 입니다.").addClass('red');
-            password[0].parentNode.parentNode.style.borderColor = "#ff5a5f";
-        }
-
-        if(passwordVal.length >= 8) { //비밀번호가 8이상일 때
-            if (passwordVal === '') { //'비밀번호'가 빈칸이면
-                passwordConfError.hide(); // '일치', '불일치'에 대한 안내 안함
-            }else if (passwordVal === passwordConfVal) {
-                passwordConfError.text("일치").addClass('green');
-                passwordConfirmation[0].parentNode.parentNode.style.borderColor = "#fff";
-            }else {
-                passwordConfError.show().text("불일치").removeClass('green').addClass('red');
-                passwordConfirmation[0].parentNode.parentNode.style.borderColor = "#ff5a5f";
-            }
-        }else{ //비밀번호가 8보다 작을 때
-            passwordConfError.hide(); //'일치', '불일치'에 대한 안내는 하지 않는다
-        }
-    });
-
-
-    //닉네임 체크 - 포커스 벗어났을 때--------------------------------------------------------
-    function checkNickname(){
+//닉네임 체크--------------------------------------------------------
+    //닉네임 함수정의
+    function checkNickname(){ //포커스 벗어났을 때
         var nicknameVal = nickname.val();
+
         if(!nicknameVal){
             nicknameError.show().text("필수정보 입니다.").addClass('red');
             nickname[0].parentNode.parentNode.parentNode.style.borderColor = "#ff5a5f";
@@ -203,18 +216,21 @@ $(document).ready(function(){
                 }
             });
         }
+    }//닉네임 함수 끝
 
-    }
 
+    //닉네임 관련 함수 호출
+    nickname.blur(function(){ //포커스를 벗어나면, 상단의 함수 실행
+        checkNickname();
+    });
+
+
+    //닉네임 예외사항 처리
     nickname.focus(function(){ //input에 포커스가 잡히면 '필수정보입니다' 안내 보여주지 않기
         nicknameError.hide();
         nickname[0].parentNode.parentNode.parentNode.style.borderColor = "#fff";
-        //nicknameCheck.hide();
+        nicknameCheck.hide();
 
-    });
-
-    nickname.blur(function(){ //포커스를 벗어나면, 상단의 함수 실행
-        checkNickname();
     });
 
     nickname.keyup(function(){ //입력값이 들어오면, 중복검사 메세지 삭제
@@ -225,7 +241,9 @@ $(document).ready(function(){
         }
     });
 
-    //대학교 및 전공 체크--------------------------------------------------------
+
+
+//대학교 및 전공 체크--------------------------------------------------------
     $('#user_university_id').change(function() { //대학교 체크여부 확인
         universityCheck = true;
         alert('대학선택!');
@@ -236,13 +254,14 @@ $(document).ready(function(){
         alert('전공선택!');
     });
 
-    //빈칸없이, 모든 유효조건이 맞는지--------------------------------------------------------
-    // newUserForm.submit(function(){
-    //     if(2>3){ //함수로 변경후 여기 조건 바꾸기
-    //         return true;
-    //     }else{
-    //         return false;
-    //     }
-    // });
 
+
+//빈칸없이, 모든 유효조건이 맞는지 체크--------------------------------------------------------
+    newUserForm.submit(function(){
+        if(2>3){ //함수로 변경후 여기 조건 바꾸기
+            return true;
+        }else{
+            return false;
+        }
+    });
 }); //end document ready
