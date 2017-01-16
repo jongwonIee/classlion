3c14e3a7e24ea3c075ba71cf7fedc08035bf1bb6
missAHNG <awr0607@gmail.com>
2017-01-17 04:28:04 +0900

    front (nav, login etc.)

--- app/assets/javascripts/sessions.js
+++ app/assets/javascripts/sessions.js
@@ -1,5 +1,4 @@
 $(document).ready(function(){
-
 //전체적으로 적용될 코드
     //input에 focus가 왔을 때, css변경 (불투명 색상)
     $('.login_input').focusin(function(){
---
+++ public/assets/sessions-54253ad07249bb5022647984031023405bef580c1f4d839f01c3c23bfa82b6e9.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+//전체적으로 적용될 코드
+    //input에 focus가 왔을 때, css변경 (불투명 색상)
+    $('.login_input').focusin(function(){
+        $(this).addClass('focus');
+    }).focusout(function() {
+        $(this).removeClass('focus');
+    });
+});
