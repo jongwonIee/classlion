9e6018171410ff2d29445fae326cc7e734e6dc8f
lee <ljw3728@gmail.com>
2017-01-18 15:18:52 +0900

    main_bg asset precompile test

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -31,6 +31,8 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
+  config.assets.precompile += %w[main_bg.png]
+
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -10,3 +10,5 @@ Rails.application.config.assets.version = '1.0'
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
 Rails.application.config.assets.precompile += %w( nav/nav_main.js /nav/nav_jquery.menu-aim.js nav/nav_modernizr.js )
+
+Rails.application.config.assets.precompile += %w[main_bg.png]
