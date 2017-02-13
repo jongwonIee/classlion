f2ab7b7b16939502136eb8210cc8b212a760f955
lee <ljw3728@gmail.com>
2017-02-13 18:13:47 +0900

    mypage backend complete

--- app/controllers/mypages_controller.rb
+++ app/controllers/mypages_controller.rb
@@ -3,6 +3,10 @@ class MypagesController < ApplicationController
 
   def index
     #자신이 쓴 강의평
+    @favorite_courses = []
     @evaluations = @current_user.evaluations
+    Favorite.where('user_id =?', @current_user.id).each do |f|
+      @favorite_courses << Course.find(f.course_id)
+    end
   end
 end
