e679a73f2434820e358108121cf7cf17db5f161c
missAHNG <awr0607@gmail.com>
2017-02-15 20:37:43 +0900

    comment time_ago OK

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,18 @@
+include ActionView::Helpers::DateHelper
 class Comment < ApplicationRecord
   belongs_to :evaluation
   belongs_to :user
+
+  def time_ago
+    #강평의 댓글 작성 시간
+    if (time_ago_in_words(self.created_at)) == "1분 이하"
+      return '방금 전'
+    elsif (time_ago_in_words(self.created_at)).last == '분' or (time_ago_in_words(self.created_at)).last == '간'
+      return time_ago_in_words(self.created_at) + " 전"
+    elsif (time_ago_in_words(self.created_at)).last == '일' and (time_ago_in_words(self.created_at)).chomp('일').to_i < 8
+      return time_ago_in_words(self.created_at) + " 전"
+    else
+      return ActionController::Base.helpers.time_tag(self.created_at, :format=>'%Y년 %m월 %d일')
+    end
+  end
 end
