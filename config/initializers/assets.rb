b5ff68c1ad28395c1e39211bc9a077e65245dd3b
missAHNG <awr0607@gmail.com>
2017-02-24 23:00:45 +0900

    jquery autocomplete

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -11,7 +11,7 @@ Rails.application.config.assets.version = '1.0'
 # Rails.application.config.assets.precompile += %w( search.js )
 
 # Javascript
-Rails.application.config.assets.precompile += %w(nav/nav_jquery.menu-aim.js nav/nav_modernizr.js nav/nav_main.js jquery.barrating.js)
+Rails.application.config.assets.precompile += %w(nav/nav_jquery.menu-aim.js nav/nav_modernizr.js nav/nav_main.js jquery.barrating.js jquery.autocomplete.js)
 
 # Stylesheet
 Rails.application.config.assets.precompile += %w(theme/bars-movie.css.scss theme/bootstrap-stars.css.scss theme/css-stars.css.scss theme/fontawesome-stars.css.scss theme/fontawesome-stars-o.css.scss)
