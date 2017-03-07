f3b422cce8e18f7f0934a97fd88078f45447b918
jongwonIee <ljw3728@gmail.com>
2017-03-07 19:29:48 +0900

    mypage 상황분리 complete

--- app/controllers/mypages_controller.rb
+++ app/controllers/mypages_controller.rb
@@ -4,6 +4,12 @@ class MypagesController < ApplicationController
   def index
     @evaluations = @current_user.evaluations.order(created_at: :desc)
 
+    @comments = []
+    Comment.where('user_id =?', @current_user.id).each do |c|
+      @comments << c
+    end
+    @comments.reverse!
+
     @favorite_courses = []
     Favorite.where('user_id =?', @current_user.id).each do |f|
       @favorite_courses << Course.find(f.course_id)
