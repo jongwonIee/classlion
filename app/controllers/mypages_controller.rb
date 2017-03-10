662aedfa5232a08a152ffc32b274709feb0d448c
jongwonIee <ljw3728@gmail.com>
2017-03-10 23:35:42 +0900

    mypages controller refactor

--- app/controllers/mypages_controller.rb
+++ app/controllers/mypages_controller.rb
@@ -4,18 +4,13 @@ class MypagesController < ApplicationController
   def index
     @evaluations = @current_user.evaluations.order(created_at: :desc)
 
-    @comments = []
-    Comment.where('user_id =?', @current_user.id).each do |c|
-      @comments << c
-    end
-    @comments.reverse!
+    @comments = @current_user.comments.reverse
 
     @favorite_courses = []
-    Favorite.where('user_id =?', @current_user.id).each do |f|
+    @current_user.favorites.each do |f|
       @favorite_courses << Course.find(f.course_id)
     end
     @favorite_courses.reverse!
-
-    @reports = Report.where('user_id =?', @current_user.id).order(created_at: :desc)
+    
   end
 end
