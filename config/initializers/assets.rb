695747f7ebf540b33a811c73118a7a01b4c86dde
yoojinn93 <yoojinn93@naver.com>
2017-05-11 07:56:51 +0000

    css layout 수정 / 메인페이지 새로운 프론트 입힘

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -17,6 +17,7 @@ Rails.application.config.assets.precompile += %w(tinymce/tinymce.min.js tinymce/
 # Stylesheet
 Rails.application.config.assets.precompile += %w(theme/bars-movie.css.scss theme/bootstrap-stars.css.scss theme/css-stars.css.scss theme/fontawesome-stars.css.scss theme/fontawesome-stars-o.css.scss)
 Rails.application.config.assets.precompile += %w(nav_reset.css.scss nav_style.css.scss style.css.scss)
+Rails.application.config.assets.precompile += %w( mySessions.js )
 # cd-arrow.svg cd-logo.svg cd-nav-icons.svg cd-search.svg lion_1.svg lion_icon.svg mail.svg mail_icon.svg nav_logo.svg sav_logo_mini.svg
 # nav_off.svg nav_on.svg nav_point.svg nav_user.svg reply.svg siren.svg
 # )
