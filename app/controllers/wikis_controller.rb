47b72d61292b91edd9c103a74e63362dcaa786d1
iamdoo2 <doo2@likelion.net>
2017-05-12 16:11:19 +0700

    auth

--- app/controllers/wikis_controller.rb
+++ app/controllers/wikis_controller.rb
@@ -1,11 +1,18 @@
 class WikisController < ApplicationController
 
   def send_wiki
+    tries = 3
     wiki = Wiki.new(      user: current_user, 
                      course_id: params[:course_id],
                           body: params[:content])
+  begin
     wiki.get_new_revision
-    wiki.save
+    wiki.save!
     render json: {:message => "success"}, :status => :ok
+  rescue ActiveRecord::RecordInvalid
+    sleep((Random.rand(10) + 1) / 10.0)
+    retry unless (tries -= 1) <= 0
+    render json: {:message => "fail"}, :status => :bad_request
+  end
   end
 end
