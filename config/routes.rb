6469ece30e4bb18e8957dbec6480fed29db1a300
iamdoo2 <doo2@likelion.net>
2017-05-14 00:31:57 +0700

    fix #6

--- config/routes.rb
+++ config/routes.rb
@@ -13,12 +13,12 @@ Rails.application.routes.draw do
   get '/info' => 'evaluations#info'
 
   #회원가입, 로그인
-  get  '/signup',  to: 'users#new'
-  post '/signup',  to: 'users#create'
-  get 'sessions/new'
-  get    '/login',   to: 'sessions#new'
-  post   '/login',   to: 'sessions#create'
-  delete '/logout',  to: 'sessions#destroy'
+  get   '/signup',  to: 'users#new'
+  post  '/signup',  to: 'users#create'
+  get   'sessions/new'
+  get   '/login',   to: 'sessions#new'
+  post  '/login',   to: 'sessions#create'
+  get   '/logout',  to: 'sessions#destroy'
   resources :users
   resources :account_activations, only: [:edit]
   resources :password_resets, only: [:new, :create, :edit, :update]
