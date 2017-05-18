568074cea4a24ac584de49e077968ec0b78639b2
iamdoo2 <doo2@likelion.net>
2017-05-18 16:07:56 +0700

    evaluation_id은 null이면 안됨

--- db/migrate/20170425141618_create_likes.rb
+++ db/migrate/20170425141618_create_likes.rb
@@ -3,7 +3,7 @@ class CreateLikes < ActiveRecord::Migration[5.1]
     create_table :likes do |t|
       t.integer :user_id,       null: false
       t.integer :course_id,     null: false
-      t.integer :evaluation_id
+      t.integer :evaluation_id, null: false
       t.boolean :is_like,       null: false
 
       t.timestamps
