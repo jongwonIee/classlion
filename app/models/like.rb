93bffe1a00653c499eca761a50231537bac5ff09
jongwonIee <ljw3728@gmail.com>
2017-05-18 16:34:00 +0900

    before create like

--- app/models/like.rb
+++ app/models/like.rb
@@ -4,4 +4,15 @@ class Like < ApplicationRecord
   belongs_to :user
   has_one :evaluation
 
+  before_create :increase_evaluation_count, :increase_user_point
+  before_destroy :decrease_evaluation_count
+
+  def increase_like_count
+
+  end
+
+  def decrease_like_count
+
+  end
+
 end
